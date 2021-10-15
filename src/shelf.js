6824d14ac2d26781470b2fc657140bb2588e7e68
Jasmyn Hicks <jasmynh44@gmail.com>
2021-10-15 14:07:47 -0600

    Add limit to num of books on shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,23 @@
+function shelfBook(book, shelf) {
+    //if (book.genre === "sciFi") {
+    if (shelf.length < 3) {
+      shelf.unshift(book)
+    }
+  //}
+  return shelf
+}
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
 };
+
+//
+// var book = {
+//   title: title,
+//   mainCharacter:
+//   pageCount:
+//   genre:
+// }
