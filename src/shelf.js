76e245e12144033fd5559782b4028ac2aeff4e8b
Jasmyn Hicks <jasmynh44@gmail.com>
2021-10-15 17:39:22 -0600

    List books on shelf in string format

--- src/shelf.js
+++ src/shelf.js
@@ -7,14 +7,41 @@ function shelfBook(book, shelf) {
   return shelf
 }
 
-function unshelfBook() {
+function unshelfBook(title, shelf) {
+  // find index # of book we want to remove
+ //  var bookIndex = shelf.indexOf(book)
+ // //use splice method to remove book from shelf
+ //  shelf.splice(bookIndex, 1)
+ //  //return the array so we can see if it worked
+ //  return shelf
 
+
+ //or try looping through
+ //for each iteration, access the element
+ //compare if equals the book in the argument
+ //if it does, remove it
+
+ for (var i = 0; i < shelf.length; i++) {
+   if (shelf[i].title === title) {
+     shelf.splice(i, 1)
+   }
+ }
 }
+
+function listTitles (shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+     titles.push(shelf[i].title)
+  }
+  var finalList = `${titles[0]}, ${titles[1]}, ${titles[2]}`
+  return finalList
+}
+
 
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
+  listTitles,
   // searchShelf
 };
 
