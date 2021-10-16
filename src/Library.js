2e07b650b10a36dd30ca824d41080e4449dfc730
Jasmyn Hicks <jasmynh44@gmail.com>
2021-10-15 20:37:44 -0600

    Add feature to automatically add books to proper shelf

--- src/Library.js
+++ src/Library.js
@@ -6,13 +6,22 @@ function createLibrary(libraryName) {
        fiction: [],
        nonFiction: []
      }
-
   }
+}
 
+function addBook(library, book) {
+  var genre = book.genre
+  if (genre === "fantasy") {
+    library.shelves.fantasy.push(book)
+  } else if (genre === "fiction") {
+    library.shelves.fiction.push(book)
+  } else if (genre === "nonFiction") {
+    library.shelves.nonFiction.push(book)
+  }
 }
 
 module.exports = {
   createLibrary,
-  // addBook,
+  addBook,
   // checkoutBook
 };
