7b535342d7bd8836658f94fd91552edbfffe71a9
Jasmyn Hicks <jasmynh44@gmail.com>
2021-10-17 21:55:28 -0600

    Add error message if book is already checked out

--- src/Library.js
+++ src/Library.js
@@ -1,5 +1,5 @@
 function createLibrary(libraryName) {
-   return {
+   var library = {
      name: libraryName,
      shelves: {
        fantasy: [],
@@ -7,6 +7,7 @@ function createLibrary(libraryName) {
        nonFiction: []
      }
   }
+  return library
 }
 
 function addBook(library, book) {
@@ -20,8 +21,58 @@ function addBook(library, book) {
   }
 }
 
+function checkoutBook(library, bookTitle, genre) {
+
+//convert "genre" into a specific book shelf
+//appearantly this is unnecessary becuase genre automatically converts to shelf
+var shelfOfBook
+
+if (genre === "fantasy") {
+  shelfOfBook = library.shelves.fantasy
+} else if (genre === "fiction") {
+  shelfOfBook = library.shelves.fiction
+} else {
+  shelfOfBook = library.shelves.nonFiction
+}
+
+//use the bookTitle to find the index number of the book
+//Array.prototype.findIndex()
+// var indexofBook = library.shelves.shelfOfBook.indexOf(bookTitle)
+
+var indexofBook = shelfOfBook.indexOf(bookTitle)
+
+//use splice to remove book from it's position in the array given the condiditon that the book is already on the shelf
+
+for (var i = 0; i < shelfOfBook.length; i++) {
+  if (shelfOfBook[i].title === bookTitle) {
+    var checkoutBook = shelfOfBook.splice(indexofBook, 1)
+    return `You have now checked out ${bookTitle} from the ${library.name}`
+ }
+}
+return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+//
+}
+
+
+// if (shelfOfBook.title.includes(bookTitle)) {
+//     shelfOfBook.splice(indexofBook, 1)
+//     return (`You have now checked out ${bookTitle} from the ${library.name}`)
+// }
+// return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+//
+// }
+
+
+//checkOutBook = shelfOfBook.splice(indexofBook, 1)
+
+
+//shelfOfBook.splice(indexofBook, 1)
+
+//print message
+
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
